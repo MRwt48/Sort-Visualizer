35c519a5117bef7a17b9dcc8ecca426a68889dce
Mukul Rawat <mukul.rawat.3748@gmail.com>
2020-10-06 10:38:40 +0530

    Basic Layout

---
+++ src/container/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Layout extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Nav</div>
+        <div>Sorting App</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
